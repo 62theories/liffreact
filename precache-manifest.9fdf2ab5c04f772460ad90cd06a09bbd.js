97b47bedf338fcd09ee22f0d385178229507db40
ball <nattawatx86@gmail.com>
2020-05-19 07:26:42 +0700

    Updates

---
+++ precache-manifest.9fdf2ab5c04f772460ad90cd06a09bbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63c1d787105a332c8ab98a817789e08",
+    "url": "/liffreact/index.html"
+  },
+  {
+    "revision": "84260b5f4effa476b3f8",
+    "url": "/liffreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36cf104db96389face65",
+    "url": "/liffreact/static/js/2.522202ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/liffreact/static/js/2.522202ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84260b5f4effa476b3f8",
+    "url": "/liffreact/static/js/main.837c1c01.chunk.js"
+  },
+  {
+    "revision": "3a791447cedd04b1ed2a",
+    "url": "/liffreact/static/js/runtime-main.0a2b93c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/liffreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
